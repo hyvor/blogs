430ab82c47c030ea88e5857d23f98a0b637ebf79
Rasif The Legend <rasif.outdesk@gmail.com>
2022-02-01 19:05:58 +0530

    dileveryAPI Update part-2

--- tests/Feature/ConsoleAPI/RedirectTest.php
+++ tests/Feature/ConsoleAPI/RedirectTest.php
@@ -17,58 +17,51 @@ class RedirectTest extends TestCase
 
     public function test_redirect_data()
     {
-        $response = $this->get('http://blogs.hyvor.test/console/test/settings/redirect');
-        $response->assertStatus(200); 
-        // /api/console/v0/blog/test/redirect
-        // $this->json('get', 'http://blogs.hyvor.test/api/console/v0/blog/supun/redirect')
-        // ->assertStatus(200)
-        // ->assertJsonStructure(
-        //     [
-        //         "old_url" => "hello world",
-        //         "new_url" => "yes hello world testing",
-        //         "type" => "301"
-                
-        //     ]
-        // );
+        // $response = $this->GET('http://blogs.hyvor.test/api/console/v0/blog/test/redirect');
+        // $response->assertStatus(200);
+
+        $response = $this->GET('http://blogs.hyvor.test/console/test/settings/redirects');
+        $response->assertStatus(200);
+
     }
 
-    // public function test_redirect_validation()
-    // {
-    //     $redirect = Redirect::make([
-    //         'old_url' => 'Testing redirects',
-    //         'new_url' => 'this is the new url'
-    //     ]);
-    //     $this->assertTrue($redirect->old_url != $redirect->new_url);
-    // }
+    public function test_redirect_validation()
+    {
+        $redirect = Redirect::make([
+            'old_url' => 'Testing redirects',
+            'new_url' => 'this is the new url'
+        ]);
+        $this->assertTrue($redirect->old_url != $redirect->new_url);
+    }
 
     // never touch
-    // public function test_delete_redirect()
-    // {
-    //     $redirect = Redirect::factory()->count(1)->make();
-    //     $redirect = Redirect::first();
-    //     if($redirect){
-    //         $redirect->delete();
-    //     }
-    //     $this->assertTrue(true);
-    // }
+    public function test_delete_redirect()
+    {
+        $response = $this->DELETE('http://blogs.hyvor.test/console/test/settings/redirects/51');
+        $response->assertStatus(405);
+    }
 
-    // public function test_create_new_redirects()
-    // {
-    //     $response = $this->post('/api/console/v0/blog/test/redirects',[
-    //         'old_url' => 'testing create new redirect',
-    //         'new_url' => 'test completed',
-    //         'type' => 301
-    //     ]);
+    public function test_create_new_redirects()
+    {
+        $response = $this->post('http://blogs.hyvor.test/console/test/settings/redirects/',[
+            'old_url' => 'testing create new redirect',
+            'new_url' => 'test completed',
+            'type' => 301
+        ]);
 
-    //     $response->assertStatus(404);
-    // }
+        $response->assertStatus(405);
+    }
 
-    // public function test_redirects_update()
-    // {
-    //     $response = $this->put('http://blogs.hyvor.test/console/supun/settings/redirects');
+    public function test_redirects_update()
+    {
+        $response = $this->put('http://blogs.hyvor.test/console/test/settings/redirects/51',[
+            'old_url' => 'testing update new redirect',
+            'new_url' => 'test new update completed',
+            'type' => 301
+        ]);
 
-    //     $response->assertStatus(200);
-    // }
+        $response->assertStatus(405);
+    }
 
     // this is to check whether the database is linked properly.
     // public function test_redirect_database(){
