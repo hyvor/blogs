f8cb9106fd72e671315b3c6819c8533006fd37db
SupunKavinda <supunkavinda1125@gmail.com>
2022-02-02 09:24:28 +0100

    users/posts count of blgos

--- app/Data/Objects/ConsoleAPI/UserBlog/UserBlogBlogObject.php
+++ app/Data/Objects/ConsoleAPI/UserBlog/UserBlogBlogObject.php
@@ -2,7 +2,9 @@
 
 namespace App\Data\Objects\ConsoleAPI\UserBlog;
 
+use App\Data\Enums\CountEnum;
 use App\Data\Objects\ConsoleAPI\BlogSubscription\SubscriptionObject;
+use App\Domains\Count\CountRepository;
 use App\Models\Blog;
 
 class UserBlogBlogObject
@@ -11,12 +13,20 @@ class UserBlogBlogObject
     public string $name;
     public string $subdomain;
     public int $posts_count;
-    public int $users_count = 200;
+    public int $users_count;
 
     public bool $is_on_trial;
     public ?int $trial_ends_at;
 
     public bool $subscribed = false;
+
+    /**
+     * This is the last subscription
+     * not the active subscription
+     * it can be a deleted subscription
+     * 
+     * Therefore use $this->subscribed to make sure the user is subscribed
+     */
     public ?SubscriptionObject $subscription = null;
 
     public function __construct(Blog $blog)
@@ -27,16 +37,15 @@ class UserBlogBlogObject
         $this->name = $blog->name;
         $this->subdomain = $blog->subdomain;
         $this->plan = $plan;
-        $this->posts_count = $blog->posts_count;
 
-        $this->is_on_trial = false; // $blog->onTrial();
+        $counts = CountRepository::getCounts($blog, ['users', 'posts']);
+        $this->posts_count = $counts['users'];
+        $this->users_count = $counts['posts'];
+
+        $this->is_on_trial = $blog->onTrial();
         $this->trial_ends_at = $blog->customer->trial_ends_at?->timestamp;
         $this->subscribed = $blog->subscribed();
 
-        /**
-         * This is the last subscription
-         * not the active subscription
-         */
         $subscription = $blog->subscription();
 
         if ($subscription) {
