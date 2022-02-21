a99583a7776872f84824e9f08824f53b089c5ced
SupunKavinda <supunkavinda1125@gmail.com>
2022-02-21 13:07:49 +0100

    heading ID

--- resources/js/console/Posts/ProseMirror/nodeview-heading.js
+++ resources/js/console/Posts/ProseMirror/nodeview-heading.js
@@ -2,24 +2,48 @@
 
 export default class Heading {
 
-    constructor(node) {
-        this.dom = this.contentDOM = document.createElement("div");
+    constructor(node, view, getPos) {
+        const _self = this
+
+        this.dom = document.createElement("div");
         this.dom.className = "heading-wrap";
 
-        const h = document.createElement("h" + node.attrs.level);
-        this.dom = this.contentDOM = document.createElement("h" + node.attrs.level)
-        if (node.content.size == 0) this.dom.classList.add("empty")
+        this.contentDOM = document.createElement("h" + node.attrs.level)
+        const id = node.attrs.id || "";
+        this.contentDOM.id = id
+        this.dom.appendChild(this.contentDOM);
 
-        this.dom.addEventListener("click", function() {
-            console.log("I am focused")
-        })
+        this.inputWrap = document.createElement("div");
+        this.inputWrap.contentEditable = false;
+        this.dom.appendChild(this.inputWrap)
+
+        // id input
+        this.input = document.createElement("input");
+        this.input.value = id;
+
+        this.input.oninput = function(e) {
+            view.dispatch(
+                view.state.tr.setNodeMarkup(
+                    getPos(),
+                    null,
+                    {...node.attrs, id: e.target.value }
+                )
+            )
+        }
+
+        this.inputWrap.appendChild(this.input);
     }
 
     update(node) {
-        if (node.type.name != "figcaption") return false
-        if (node.content.size > 0) this.dom.classList.remove("empty")
-        else this.dom.classList.add("empty")
-        return true
+        if (node.type.name === 'heading') {
+            this.contentDOM.id = node.attrs.id;
+            this.input.value = node.attrs.id;
+            return true;
+        }
+    }
+
+    stopEvent(e) {
+        return e.target.isEqualNode(this.input)
     }
 
 }
