b3846282b1e92661d744a84ba7ffb2bf89c44a3b
SupunKavinda <supunkavinda1125@gmail.com>
2022-07-23 11:23:12 +0200

    remove countable from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    use Countable;
     use HasFactory;
 
     protected $casts = [
@@ -38,10 +37,4 @@ class User extends Model
         return $this->hasOne(Media::class, 'picture_id');
     }
 
-    function countsDefinition()
-    {
-        return [
-            'posts'
-        ];
-    }
 }
