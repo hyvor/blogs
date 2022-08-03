56911d6ede86fac8b9a5a1258d54aa49b983f9ab
SupunKavinda <supunkavinda1125@gmail.com>
2022-08-03 09:56:53 +0200

    download themes command, and minor production fixes

---
+++ app/Console/Commands/DownloadThemes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Domains\Theme\GithubSync\GithubSyncService;
+use Illuminate\Console\Command;
+
+class DownloadThemes extends Command
+{
+    protected $signature = 'download:themes';
+    protected $description = 'Download themes';
+
+    public function handle()
+    {
+        $this->comment('Downloading themes');
+        GithubSyncService::syncFromGithubZipBall();
+        $this->info('Themes downloaded');
+    }
+}
