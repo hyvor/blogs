2784a972a6d2e1fdd5670eb1eac6f509b3f14b14
SupunKavinda <supunkavinda1125@gmail.com>
2022-09-08 11:03:55 +0530

    responsive some fixes

--- resources/views/landing/nav.blade.php
+++ resources/views/landing/nav.blade.php
@@ -13,7 +13,31 @@
 				<a class="nav-item nav-link{{ request()->is('pricing') ? " active" : ""  }}" href="/pricing">Pricing</a>
 				<a class="nav-item nav-link" href="/console" data-flashload-skip-link>Console</a>
 			</div>
+			<button id="nav-mobile" onclick="toggleNavBar(event)">
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+					<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+				</svg>
+			</button>
 		</div>
 	</div>
+
+	<script>
+		function toggleNavBar(e) {
+			var navbar = document.querySelector('.navbar-nav');
+			e && e.stopPropagation();
+			if (navbar.classList.contains('open')) {
+				navbar.classList.remove('open');
+				window.removeEventListener('click', handleNavBarOut);
+			} else {
+				navbar.classList.add('open')
+				window.addEventListener('click', handleNavBarOut)
+			}
+		}
+		function handleNavBarOut(e) {
+			if (!document.querySelector('.navbar-nav').contains(e.target)) {
+				toggleNavBar();
+			}
+		}
+	</script>
 
 </nav>
