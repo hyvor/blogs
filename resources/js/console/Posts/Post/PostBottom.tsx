ac643af1c090b80c31ed6a70006bc7e92e765235
SupunKavinda <supunkavinda1125@gmail.com>
2022-12-29 00:25:59 +0000

    saved

--- resources/js/console/Posts/Post/PostBottom.tsx
+++ resources/js/console/Posts/Post/PostBottom.tsx
@@ -1,20 +1,36 @@
 import {InfoCircle} from "react-bootstrap-icons";
 import React from "react";
-import {usePostValues} from "./helpers";
+import {usePostActions, usePostValues} from "./helpers";
 
 export default function PostBottom({id} : {id:number}) {
 
-    const { savePostAjax  } = usePostValues(id)
+    const { savePostAjax, diff  } = usePostValues(id)
+    const { savePost } = usePostActions(id)
+
+    const hasChanges = Object.keys(diff).length > 0
 
     return <div
         className="post-editor-bottom"
     >
         <div className="post-editor-bottom-content">
             <div className="right">
-                {
-                    savePostAjax.status === 'loading' ?
-                        <span className="saving">Saving...</span> : null
-                }
+
+                <span className="saver">
+
+                    {
+                        savePostAjax.status === 'loading' ?
+                            "Saving..." :
+
+                            (
+                                hasChanges ?
+                                    <span className="not-saved" onClick={savePost}>Unsaved changes *</span> :
+                                    <span className="saved">Saved</span>
+                            )
+
+                    }
+
+                </span>
+
                 <span className="words" id="pm-word-count"/>
                 <a target="_blank" href="/docs/writing" className="help">
                     <InfoCircle />
