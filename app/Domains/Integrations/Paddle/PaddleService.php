f1e6b2f52cb30071b943f46bcc672c5a3c020ef1
SupunKavinda <supunkavinda1125@gmail.com>
2023-03-30 19:47:11 +0200

    paylink tests

--- app/Domains/Integrations/Paddle/PaddleService.php
+++ app/Domains/Integrations/Paddle/PaddleService.php
@@ -18,7 +18,7 @@ class PaddleService
 
     public function createActivationPayLink(Blog $blog) {
         $data = PaddleApiCaller::call('/product/generate_pay_link', [
-            'product_id' => 48210,
+            'product_id' => config('services.paddle.activation_plan_id'),
             'passthrough' => Passthrough::encode($blog),
             'quantity_variable' => 0,
         ]);
