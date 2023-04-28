e55434ba706ceed8a585d2a02e1178a24b9e69b8
SupunKavinda <supunkavinda1125@gmail.com>
2023-04-28 02:27:06 +0200

    unsaved changes fix

--- resources/js/console/Posts/Post/PostBottom.tsx
+++ resources/js/console/Posts/Post/PostBottom.tsx
@@ -7,7 +7,7 @@ export default function PostBottom({id} : {id:number}) {
     const { savePostAjax, diff, currentVariant  } = usePostValues(id)
     const { savePost } = usePostActions(id)
 
-    const hasChanges = Object.keys(diff).length > 0 || currentVariant.content_unsaved !== currentVariant.content && currentVariant.content_unsaved !== null;
+    const hasChanges = Object.keys(diff).length > 0;
 
     return <div
         className="post-editor-bottom"
