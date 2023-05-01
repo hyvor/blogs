8a67af68b162a5d1ac07bc3a0cef3fd6775414cc
SupunKavinda <supunkavinda1125@gmail.com>
2023-05-01 03:32:46 +0200

    ci

--- tests/MeilisearchInefficient.php
+++ tests/MeilisearchInefficient.php
@@ -16,13 +16,19 @@ class MeilisearchInefficient
             strval(config('scout.meilisearch.host')),
             strval(config('scout.meilisearch.key'))
         );
-        dump($client->getTasks()->toArray()['results']);
+
         while (
             collect($client->getTasks()->toArray()['results'])
                 ->filter(fn($task) => $task['status'] === 'enqueued' || $task['status'] === 'processing')
                 ->count()
             > 0
         ) {
+
+            dump($client->getTasks()->toArray()['results']);
+            dump(collect($client->getTasks()->toArray()['results'])
+                ->filter(fn($task) => $task['status'] === 'enqueued' || $task['status'] === 'processing')
+                ->count());
+
             // 250ms
             usleep(250000);
         }
