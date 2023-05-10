63607f38bb422f99e5ccfffb6ef725521a4a0f41
Nolab <thibault.boutet@epita.fr>
2023-05-10 15:54:44 +0200

    Remove all hardcoded color

--- resources/js/console/Left/BlogsSelector.jsx
+++ resources/js/console/Left/BlogsSelector.jsx
@@ -66,6 +66,18 @@ export default function BlogsSelector() {
         sortableRef.current.classList.remove("dragging");
     }
 
+    const [mousePos, setMousePos] = useState({});
+    useEffect(() => {
+        function handleMouseMove(e) {
+            setMousePos({x: e.clientX, y: e.clientY});
+            console.log('Mouse move', e.clientX, e.clientY);
+        }
+        window.addEventListener("mousemove", handleMouseMove);
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        }
+    });
+
     return <div className="blog-selector">
         <div className="value" onClick={isListOpen ? null : openList}>
             <div className="name">{ activeBlog.blog.name }</div>
@@ -94,7 +106,7 @@ export default function BlogsSelector() {
                      */
                     forceFallback={true}
 
-                    animation={200}
+                    animation={0}
                 >
                     {
                         blogs.map(({blog, user}) => {
