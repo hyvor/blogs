2eb612e10eee0c59339959a26804626418e04ae0
Nolab <thibault.boutet@epita.fr>
2023-05-11 16:40:08 +0200

    Fix bug selection on home page

--- resources/js/console/Welcome/BlogsList.tsx
+++ resources/js/console/Welcome/BlogsList.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import { useValues } from "kea";
+import React, { useRef } from 'react'
+import { useActions, useValues } from "kea";
 import userBlogsLogic from "../logic/userBlogsLogic";
 import { UserBlog } from '../objects/userblog';
 import NavLink from "../ReusableComponents/NavLink";
 import { BlogType } from "../enums";
+import subdomainLogic from '../logic/subdomainLogic';
 
 export default function BlogsList() {
 
     const { blogs } = useValues(userBlogsLogic());
+    const { setSubdomain } = useActions(subdomainLogic);
+
+    function handleBlogChange(subdomain: string) {
+        setSubdomain(subdomain, null, true);
+    }
 
     return <div className="blogs-list">
 
@@ -27,6 +33,7 @@ export default function BlogsList() {
                     return <NavLink
                         key={blog.id}
                         href={"/console/" + blog.subdomain}
+                        onClick={() => {handleBlogChange(blog.subdomain)}}
                         className={"blog-card" + (blog.type === BlogType.DEV ? " dev" : "")}
                     >
 
