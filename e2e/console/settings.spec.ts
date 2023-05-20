ab962a3c0029821bfde76955a15d3c8edc98b94c
Nolab <thibault.boutet@epita.fr>
2023-05-20 10:04:14 +0200

    Add test for a second language

--- e2e/console/settings.spec.ts
+++ e2e/console/settings.spec.ts
@@ -5,15 +5,18 @@ import {consoleTest} from "./consoleTest.ts";
 test.describe('settings', () => {
 
     consoleTest('General', async ({testingApi, console, page}) => {
-        await testingApi.factory.blogFull();
+
+        // Database seeding
+        const blog = await testingApi.factory.blogFull();
+        await testingApi.factory.language({blog_id: blog.blog.id, name: 'English', code: 'en', is_primary: false});
+
         await console.visitAndNav('settings');
 
         await expect(page.getByText('General Settings')).toBeVisible();
 
+        // Fill main language
         await page.locator('#input-name').fill('MyAwesome Blog');
-
         await page.locator('#input-description').fill('MyAwesomeDescription');
-
         await page.locator('#input-blog-facebook').fill('facebook');
         await page.locator('#input-blog-twitter').fill('twitter');
         await page.locator('#input-blog-linkedin').fill('linkedin');
@@ -21,10 +24,17 @@ test.describe('settings', () => {
         await page.locator('#input-blog-tiktok').fill('tiktok');
         await page.locator('#input-blog-instagram').fill('instagram');
         await page.locator('#input-github').fill('github');
-
         await page.getByRole('button', { name: 'SAVE' }).click();
         await page.reload();
 
+        // Fill secondary language
+        await page.locator('span').filter({ hasText: 'en' }).first().click();
+        await page.waitForTimeout(2000);
+        await page.locator('#input-name').fill('EnglishBlog');
+        await page.locator('#input-description').fill('EnglishDescription');
+        await page.getByRole('button', { name: 'SAVE' }).click();
+
+        // Test main language
         await expect(page.locator('#input-name')).toHaveValue('MyAwesome Blog');
         await expect(page.locator('#input-description')).toHaveValue('MyAwesomeDescription');
         await expect(page.locator('#input-blog-facebook')).toHaveValue('facebook');
@@ -34,6 +44,11 @@ test.describe('settings', () => {
         await expect(page.locator('#input-blog-tiktok')).toHaveValue('tiktok');
         await expect(page.locator('#input-blog-instagram')).toHaveValue('instagram');
         await expect(page.locator('#input-github')).toHaveValue('github');
+
+        // Test secondary language
+        await page.locator('span').filter({ hasText: 'en' }).first().click();
+        await expect(page.locator('#input-name')).toHaveValue('EnglishBlog');
+        await expect(page.locator('#input-description')).toHaveValue('EnglishDescription');
     });
 
     consoleTest('Users: adding blog user', async ({testingApi, console, page}) => {
