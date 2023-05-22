bfd5abff445799375d8564bca1aa9556f7891834
Nolab <thibault.boutet@epita.fr>
2023-05-22 09:51:45 +0200

    Add hosting test

--- e2e/console/settings.spec.ts
+++ e2e/console/settings.spec.ts
@@ -86,5 +86,53 @@ test.describe('Settings', () => {
         });
     })
 
+    test.describe('Hosting', () => {
+
+        consoleTest.beforeEach(async ({testingApi, console, page}) => {
+            await testingApi.factory.blogFull();
+            await console.visitAndNav('settings');
+            await page.getByRole('link', { name: 'Hosting' }).click();
+          });
+
+        consoleTest('Subdomain modification', async ({testingApi, console, page}) => {
+            await page.getByLabel('', { exact: true }).fill('subdomain');
+            await page.getByRole('button', { name: 'SAVE' }).click();
+            await page.reload();
+            await expect(page.getByText('subdomain')).toBeVisible(); 
+        });
+
+        consoleTest('Custom domain', async ({testingApi, console, page}) => {
+            await page.locator('label').filter({ hasText: 'Custom Domain' }).locator('span').nth(1).click();
+            await page.locator('#input-custom-domain').fill('https://www.google.com/');
+            await page.getByRole('button', { name: 'SAVE' }).click();
+            await page.reload();
+            await expect(page.locator('label').filter({ hasText: 'Subdomain' }).locator('span').nth(1)).not.toBeChecked();
+            await expect(page.locator('label').filter({ hasText: 'Custom Domain' }).locator('span').nth(1)).toBeChecked();
+            await expect(page.locator('#input-custom-domain')).toHaveValue('https://www.google.com/');
+        });
+
+        consoleTest('Self-hosting', async ({testingApi, console, page}) => {
+            await page.locator('label').filter({ hasText: 'Self-hosting' }).locator('span').nth(1).click();
+            await page.locator('#input-self-hosting-url').fill('https://www.google.com/');
+            await page.getByRole('button', { name: 'SAVE' }).click();
+            await page.reload();
+            await expect(page.locator('label').filter({ hasText: 'Subdomain' }).locator('span').nth(1)).not.toBeChecked();
+            await expect(page.locator('label').filter({ hasText: 'Self-hosting' }).locator('span').nth(1)).toBeChecked();
+            await expect(page.locator('#input-self-hosting-url')).toHaveValue('https://www.google.com/');
+        });
+
+        consoleTest('Embeddable', async ({testingApi, console, page}) => {
+            await page.getByTestId('switch').locator('div').nth(2).click();
+            await page.locator('#input-self-hosting-url').fill('https://www.google.com/');
+            await page.locator('#input-self-hosting-url').click({
+              modifiers: ['Control']
+            });
+            await page.getByRole('button', { name: 'SAVE' }).click();
+            await page.reload();
+            await expect(page.locator('#input-self-hosting-url')).toHaveValue('https://www.google.com/');
+
+        });
+    });
+
 
 });
