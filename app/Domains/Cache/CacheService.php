cdaff3ef1c430d974b428119821ce820628a5d2f
SupunKavinda <supunkavinda1125@gmail.com>
2023-06-25 14:15:45 +0200

    lang cache update

--- app/Domains/Cache/CacheService.php
+++ app/Domains/Cache/CacheService.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Domains\Cache;
 
@@ -35,7 +35,7 @@ class CacheService
         return "blog_cache_{$this->blog->id}_$path";
     }
 
-    public function clearTemplateCache()
+    public function clearTemplateCache() : void
     {
         $key = $this->getKey(self::LAST_TEMPLATE_CACHE_CLEARED_AT);
         Cache::put($key, now()->timestamp);
@@ -43,7 +43,7 @@ class CacheService
         CacheClearTemplatesEvent::dispatch($this->blog);
     }
 
-    public function clearSingleCache(string $path)
+    public function clearSingleCache(string $path) : void
     {
         $key = $this->getKey($path);
         Cache::forget($key);
@@ -51,7 +51,7 @@ class CacheService
         CacheClearSingleEvent::dispatch($this->blog, $path);
     }
 
-    public function clearAllCache()
+    public function clearAllCache() : void
     {
         $key = $this->getKey(self::LAST_ALL_CACHE_CLEARED_AT);
         Cache::put($key, now()->timestamp);
@@ -59,7 +59,7 @@ class CacheService
         CacheClearAllEvent::dispatch($this->blog);
     }
 
-    public function set(string $path, DeliveryAPIResponseObject $responseObject)
+    public function set(string $path, DeliveryAPIResponseObject $responseObject) : void
     {
         $key = $this->getKey($path);
         Cache::put($key, serialize($responseObject));
@@ -71,6 +71,10 @@ class CacheService
         $cache = Cache::get($key);
 
         if (!$cache) {
+            return null;
+        }
+
+        if (!is_string($cache)) {
             return null;
         }
 
