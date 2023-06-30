88fefc8e5e0cf7d3e3f26ef7000422772cd9f5fb
Nolab <thibault.boutet@epita.fr>
2023-06-30 11:01:20 +0200

    Export menu into react component

--- resources/js/console/Posts/Post/ProseMirror/nodeview-table.tsx
+++ resources/js/console/Posts/Post/ProseMirror/nodeview-table.tsx
@@ -21,6 +21,7 @@ import {
   } from "prosemirror-tables";
 import { NodeSelection } from "prosemirror-state";
 import { createRoot } from "react-dom/client";
+import TableMenu from "./TableMenu";
 
 
 
@@ -80,12 +81,7 @@ export default class Table implements NodeView{
 
         this.dom.appendChild(this.bottomSettings);
         this.createMenuItems();
-
-        this.view.dom.addEventListener('focus', this.handleFocusChange);
-        this.view.dom.addEventListener('blur', this.handleFocusChange);
-        this.view.dom.addEventListener('keydown', this.handleKeyDown);
-        this.view.dom.addEventListener('click', this.handleClick);
-        
+    
     }
 
     handleFocusChange = () => {
@@ -203,24 +199,12 @@ export default class Table implements NodeView{
           const row = table.content.child(rowIdx);
           const isFocused = this.isRowFocused(row);
       
-          const rowButton = document.createElement('button');
-          rowButton.className = 'row-button';
-          rowButton.innerText = `...`;
-          rowButton.style.display = isFocused ? 'block' : 'none';
+          const uploader = <TableMenu
+                   isFocused={isFocused}
+                   selectedRow={selectedRow}
+                />;
+                ReactDOM.render(uploader, this.leftSideSettings);
             
-          const tableHeight = this.view.dom.offsetHeight;
-          // Position the row button next to the selected row
-          if (selectedRow && row === selectedRow) {
-            rowButton.style.position = 'absolute';
-            rowButton.style.left = '5px'; // Adjust the positioning as needed
-            rowButton.style.top = `${rowIdx * tableHeight / 4 + tableHeight / 4}px`; // Adjust the positioning as needed
-          }
-      
-          rowButton.onclick = function () {
-            console.log(`Clicked on row ${rowIdx + 1}`);
-          };
-      
-          this.leftSideSettings.appendChild(rowButton);
         }
       }
       
