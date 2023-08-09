e3ebdfc612c3d44d925dfe9d55a21a159d6a5768
SupunKavinda <supunkavinda1125@gmail.com>
2023-08-09 13:57:08 +0200

    wip

---
+++ resources/js/console/ReusableComponents/Button.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from "react";
+
+interface ButtonProps {
+    children: ReactNode,
+    onClick?: () => void,
+    size?: 'big' | 'small' | 'tiny' | 'medium',
+    type?: 'primary' | 'inactive' | 'text-only' | 'secondary' | 'danger' | 'light' | 'orange' | 'green' | 'gray'
+}
+
+export default function Button({
+    size = 'medium',
+    type = 'primary',
+    ...props
+}: ButtonProps) {
+
+    return <button className={`button ${size} ${type}`} onClick={props.onClick}>
+        {props.children}
+    </button>
+
+}
