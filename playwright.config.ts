ed2247549b488756f5a5b95590543bfab0e76942
Nolab <thibault.boutet@epita.fr>
2023-08-24 15:39:33 +0200

    Try new timeout value

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,11 +22,11 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  globalTimeout: 10000,
+  globalTimeout: 1000000,
   timeout: 10000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:8080',
+    baseURL: 'http://127.0.0.1:8000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
