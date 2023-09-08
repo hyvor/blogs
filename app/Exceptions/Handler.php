a336bf66f530492269df0c0af6734938f6a8e45a
SupunKavinda <supunkavinda1125@gmail.com>
2023-09-08 23:03:46 +0200

    sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,7 +38,9 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if (app()->bound('sentry') && app()->environment('production')) {
+                app('sentry')->captureException($e);
+            }
         });
     }
 
