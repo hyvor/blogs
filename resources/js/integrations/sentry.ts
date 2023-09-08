12a13d280e6ebd084384c547de0bb5c027a2d072
SupunKavinda <supunkavinda1125@gmail.com>
2023-09-08 06:02:41 +0200

    sentry update

--- resources/js/integrations/sentry.ts
+++ resources/js/integrations/sentry.ts
@@ -2,7 +2,6 @@
 import * as Sentry from "@sentry/react";
 
 if (import.meta.env.PROD) {
-    
 
     Sentry.init({
         environment: 'production',
@@ -12,12 +11,12 @@ if (import.meta.env.PROD) {
                 // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
                 tracePropagationTargets: [/^https:\/\/blogs.hyvor.com/],
             }),
-            // new Sentry.Replay(),
+            new Sentry.Replay(),
         ],
         // Performance Monitoring
-        tracesSampleRate: 0.5, // Capture 100% of the transactions, reduce in production!
+        tracesSampleRate: 0, // Capture 100% of the transactions, reduce in production!
         // Session Replay
-        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysSessionSampleRate: 0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
         replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     });
 
