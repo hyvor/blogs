aeff5a8ffb70f7156517a71b14cc391b830fdfe0
Nolab <thibault.boutet@epita.fr>
2023-10-09 16:10:53 +0200

    Add common shortcuts

--- resources/js/console/Posts/Post/ProseMirror/keymap-plugin.ts
+++ resources/js/console/Posts/Post/ProseMirror/keymap-plugin.ts
@@ -5,17 +5,26 @@ import {
 } from "./commands";
 import {
     setBlockType,
-    chainCommands
+    chainCommands,
+    toggleMark,
+    joinUp,
+    joinDown,
+    lift,
+    selectParentNode,
+    wrapIn,
+    exitCode
 } from 'prosemirror-commands'
 import { undo, redo } from "prosemirror-history";
 import {
     splitListItem,
     sinkListItem,
     liftListItem,
+    wrapInList,
 } from "prosemirror-schema-list";
-import { Command, EditorState, NodeSelection, Selection } from "prosemirror-state";
+import { Command, EditorState, NodeSelection, Selection, Transaction } from "prosemirror-state";
 import { ResolvedPos, Schema } from "prosemirror-model";
 import { EditorView } from "prosemirror-view";
+import { undoInputRule } from "prosemirror-inputrules";
 
 export default function keymapPlugins(schema: Schema) {
     var extendedKeymap : Record<string, Command> = {};
@@ -197,7 +206,9 @@ export default function keymapPlugins(schema: Schema) {
         return false;
     });
 
-    return [keymap(extendedKeymap), keymap(baseKeymap), getCodeBlockKeymap()];
+    
+
+    return [keymap(extendedKeymap), keymap(baseKeymap), getCodeBlockKeymap(), getShortcutKeymap(schema)];
 }
 
 const figcaptionEnterHandler : Command = (state, dispatch) => {
@@ -289,4 +300,127 @@ function getCodeBlockKeymap() {
         ArrowUp: arrowHandler("up"),
         ArrowDown: arrowHandler("down"),
     });
+}
+
+function getShortcutKeymap(schema: Schema) {
+
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    const isMac = /macintosh|mac os x/i.test(userAgent);
+    let keyMap = {
+        "Mod-z": undo,
+        "Shift-Mod-z": redo,
+        Backspace: undoInputRule
+      };
+      if (!isMac) keyMap = { ...keyMap, ...{ "Mod-y": redo } };
+    
+      keyMap = {
+        ...keyMap,
+        ...{
+          "Alt-ArrowUp": joinUp,
+          "Alt-ArrowDown": joinDown,
+          "Mod-[": lift,
+          Escape: selectParentNode
+        }
+      };
+    
+      if (schema.marks.strong)
+        keyMap = {
+          ...keyMap,
+          ...{
+            "Mod-b": toggleMark(schema.marks.strong),
+            "Mod-B": toggleMark(schema.marks.strong)
+          }
+        };
+      if (schema.marks.em)
+        keyMap = {
+          ...keyMap,
+          ...{
+            "Mod-i": toggleMark(schema.marks.em),
+            "Mod-I": toggleMark(schema.marks.em)
+          }
+        };
+      if (schema.marks.code)
+        keyMap = {
+          ...keyMap,
+          ...{ "Mod-`": toggleMark(schema.marks.code) }
+        };
+      if (schema.nodes.bullet_list)
+        keyMap = {
+          ...keyMap,
+          ...{ "Shift-Ctrl-8": wrapInList(schema.nodes.bullet_list) }
+        };
+      if (schema.nodes.ordered_list)
+        keyMap = {
+          ...keyMap,
+          ...{ "Shift-Ctrl-9": wrapInList(schema.nodes.ordered_list) }
+        };
+      if (schema.nodes.blockquote)
+        keyMap = {
+          ...keyMap,
+          ...{ "Ctrl->": wrapIn(schema.nodes.blockquote) }
+        };
+      if (schema.nodes.hard_break) {
+        let cmd = chainCommands(exitCode, (state, dispatch) => {
+          if (!dispatch) return false;
+          dispatch(
+            state.tr
+              .replaceSelectionWith(schema.nodes.hard_break.create())
+              .scrollIntoView()
+          );
+          return true;
+        });
+    
+        keyMap = { ...keyMap, ...{ "Mod-Enter": cmd, "Shift-Enter": cmd } };
+        if (isMac) keyMap = { ...keyMap, ...{ "Ctrl-Enter": cmd } };
+      }
+      if (schema.nodes.list_item)
+        keyMap = {
+          ...keyMap,
+          ...{
+            Enter: splitListItem(schema.nodes.list_item),
+            // "Mod-[": liftListItem(schema.nodes.list_item),
+            "Mod-]": sinkListItem(schema.nodes.list_item)
+          }
+        };
+      if (schema.nodes.paragraph)
+        keyMap = {
+          ...keyMap,
+          ...{ "Shift-Ctrl-0": setBlockType(schema.nodes.paragraph) }
+        };
+      if (schema.nodes.code_block)
+        keyMap = {
+          ...keyMap,
+          ...{ "Shift-Ctrl-\\": setBlockType(schema.nodes.code_block) }
+        };
+      if (schema.nodes.heading) {
+        const headingLevels = Array.from({ length: 6 }, (_, i) => ++i);
+        const headingMap = headingLevels.reduce(
+          (acc, level) => ({
+            ...acc,
+            ...{
+              [`Shift-Ctrl-${level}`]: setBlockType(schema.nodes.heading, { level })
+            }
+          }),
+          {}
+        );
+    
+        keyMap = { ...keyMap, ...headingMap };
+      }
+    
+      if (schema.nodes.horizontal_rule)
+        keyMap = {
+          ...keyMap,
+          ...{
+            "Mod-_": (state: EditorState, dispatch: (tr: Transaction) => void) => {
+              dispatch(
+                state.tr
+                  .replaceSelectionWith(schema.nodes.horizontal_rule.create())
+                  .scrollIntoView()
+              );
+              return true;
+            }
+          }
+        };
+    
+      return keymap(keyMap);
 }
