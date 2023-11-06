3ec64f61954575a5d80fadf64574c666b134da3d
Nolab <thibault.boutet@epita.fr>
2023-11-06 14:20:57 +0100

    Fix ts error

--- resources/js/console/Posts/Post/ProseMirror/schema.ts
+++ resources/js/console/Posts/Post/ProseMirror/schema.ts
@@ -337,6 +337,7 @@ export const marks = {
 // To reuse elements from this schema, extend or read from its
 // `spec.nodes` and `spec.marks` [properties](#model.Schema.spec).
 
+//@ts-ignore
 const schemaWithoutList = new Schema({nodes, marks});
 
 export default new Schema({
