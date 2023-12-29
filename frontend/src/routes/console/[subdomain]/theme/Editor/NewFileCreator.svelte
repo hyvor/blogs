fe17c5698707a86a38235b00e97ea5d2ed7753a3
SupunKavinda <supunkavinda1125@gmail.com>
2023-12-29 16:06:50 +0100

    theme wip

---
+++ frontend/src/routes/console/[subdomain]/theme/Editor/NewFileCreator.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let uploadInput: HTMLInputElement;
+
+    function handleUpload() {
+
+    }
+</script>
+
+
+<div class="file-creator">
+
+    <input
+        type="file"
+        bind:this={uploadInput}
+        style="display: none;"
+        on:change={handleUpload}
+    />
+
+</div>
