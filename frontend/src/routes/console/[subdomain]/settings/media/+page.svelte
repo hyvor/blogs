9429853f1cd22858c5946afb7ef7e9668362d5ee
SupunKavinda <supunkavinda1125@gmail.com>
2024-01-09 20:41:46 +0100

    media

---
+++ frontend/src/routes/console/[subdomain]/settings/media/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { Callout, Link } from "@hyvor/design/components";
+	import { blogStore } from "../../../lib/stores/blogStore";
+
+</script>
+
+<div>
+<Callout type="info">
+    Media settings are coming soon with the following features.
+    <ul>
+        <li>
+            Adjust how your media (ex: images) files are served.
+        </li>
+        <li>
+            Set up S3 for storing your media files.
+        </li>
+    </ul>
+    Stay tuned!
+</Callout>
+
+<p style="font-size:14px;color:var(--text-light)">
+    Media library is now moved to <Link href={`/console/${$blogStore.subdomain}/tools/media`}>Tools &rarr; Media Library</Link>.
+</p>
+</div>
+
+<style>
+    div {
+        padding: 30px;
+    }
+</style>
