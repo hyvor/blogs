0cb96a8acc1e7b3e89c4f3b7633b29edf94391b1
SupunKavinda <supunkavinda1125@gmail.com>
2024-01-09 20:19:11 +0100

    wip

---
+++ frontend/src/routes/console/[subdomain]/settings/navigation/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Button } from "@hyvor/design/components";
+	import SettingsTop from "../@components/SettingsTop.svelte";
+	import { IconPlus } from "@hyvor/icons";
+
+    let isCreating = false;
+
+</script>
+
+
+<SettingsTop>
+
+    <Button on:click={() => isCreating = true}>
+        Create Navigation <IconPlus slot="end" />
+    </Button>
+
+</SettingsTop>
