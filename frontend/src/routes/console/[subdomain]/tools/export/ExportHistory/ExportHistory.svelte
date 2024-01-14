e6de31c2349f332f0ce5eff41609768caba41286
SupunKavinda <supunkavinda1125@gmail.com>
2024-01-14 17:15:27 +0100

    export done

---
+++ frontend/src/routes/console/[subdomain]/tools/export/ExportHistory/ExportHistory.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { IconMessage, Loader, Table, TableRow, toast } from "@hyvor/design/components";
+    import type { Export } from "../../../../lib/types";
+	import ExportRow from "./ExportRow.svelte";
+	import { getExports } from "../exportActions";
+
+    let isLoading = true;
+
+    export let exports: Export[] = [];
+
+    getExports()
+        .then(res => {
+            exports = res;
+        })
+        .catch(err => toast.error(err.message))
+        .finally(() => isLoading = false);
+
+</script>
+
+
+{#if isLoading}
+    <Loader padding={100} block />
+{:else}
+
+    {#if exports.length === 0}
+        <IconMessage empty message="No exports found" padding={60} />
+    {:else}
+
+        <Table columns="1fr 1fr 1fr 80px">
+
+            <TableRow head>
+                <div>Format</div>
+                <div>Date</div>
+                <div>Status</div>
+                <div>File</div>
+            </TableRow>
+
+            {#each exports as exp}
+                <ExportRow data={exp} />
+            {/each}
+
+        </Table>
+
+    {/if}
+
+{/if}
