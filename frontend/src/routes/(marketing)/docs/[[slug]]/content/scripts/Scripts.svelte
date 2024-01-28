cb817533ff017a68a8f6290a75c1537b05b0c99b
IshiniAvindya <ishiniavindya2000@gmail.com>
2024-01-28 17:14:13 +0100

    themes -> scripts completed #314
    wip

--- frontend/src/routes/(marketing)/docs/[[slug]]/content/scripts/Scripts.svelte
+++ frontend/src/routes/(marketing)/docs/[[slug]]/content/scripts/Scripts.svelte
@@ -19,8 +19,19 @@
     <p>If possible, try to write inline Javascript to avoid HTTP requests completely.</p>
 
     <CodeBlock code={`
-       <` + `script>
-        // my js here
-        </script>
+      <` + `script>
+         // my js here
+      </script>
     `} />
+
+      <h2 id="flashload-safe">Flashload Safe</h2>
+         <p><a href="https://github.com/hyvor/flashload">Flashload</a> does magic, but make sure you understand how it works to avoid common pitfalls. Let's say you are navigating from <code>/</code> to <code>/page</code>. Flashload prevents the browser reload and loads the page by itself and replaces <b>only the</b> <code>{`<body>`}</code>. It means,</p>
+
+         <ul>
+            <li>CSS in <code>{`<head>`}</code> is only loaded once. That is why we have a single <code>styles.css</code> stylesheet that contains all styles of the blog.</li>
+            <li><code>{`<script>`}</code>s in <code>{`<head>`}</code> will only run in the first page load, and not when navigating.</li>
+            <li><code>{`<script>`}</code>s in <code>{`<body>`}</code> will load/run on each navigation, unless they have a <b>data-flashload-skip-script</b> attribute.</li>
+         </ul>
+
+    
 
