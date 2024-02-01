dbd93ba8407ec9f622f1eaedcb81fcf39a0e39ad
SupunKavinda <supunkavinda1125@gmail.com>
2024-02-01 14:37:27 +0100

    tags docs

---
+++ frontend/src/routes/(marketing)/docs/[[slug]]/content/tags/Tags.svelte
@@ -0,0 +1,21 @@
+<h1>Tags</h1>
+
+<p>
+    Tags are used to "group" similar posts. You can assign one or more tags to a post.
+</p>
+
+
+Each tag creates a tag index page (`/tag/tag-slug`), which lists the posts of that tag. This makes easy for users to browse posts of a specific slug. For example, our [official blog](/blog) has multiple tags, and you can visit [/blog/tag/seo](/blog/tag/seo) to see SEO-related posts.
+
+<h2 id="tag-index">
+    Tag index page
+</h2>
+
+<p>
+    Each tag creates an index page (<code>/tag/tag-slug</code>), which lists the posts of that tag. This makes easy for users to browse posts of a specific tag. For example, our <a href="/blog">official blog</a> has multiple tags, and you can visit <a href="/blog/tag/seo">/blog/tag/seo</a> to see SEO-related posts.
+</p>
+
+<h2 id="add">
+    Creating a tag
+</h2>
+
