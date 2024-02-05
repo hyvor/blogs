9a9a6adbb14d0b0ec6487e006924d11e338068d5
SupunKavinda <supunkavinda1125@gmail.com>
2024-02-05 19:21:53 +0100

    adds alternative links

--- frontend/src/routes/(marketing)/+layout.svelte
+++ frontend/src/routes/(marketing)/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+	import Footer from './Footer.svelte';
 	import Header from './Header.svelte';
-    import { Footer, FooterLinkList } from '@hyvor/design/marketing';
 	import { page } from "$app/stores";
 </script>
 
@@ -11,41 +11,7 @@
 {#if $page.url.pathname !== '/themes'}
 
     <div class="footer-wrap">
-        <Footer
-            email="blogs.support@hyvor.com"
-            social={{
-                x: 'https://x.com/HyvorBlogs',
-                youtube: null,
-                discord: null,
-            }}
-        >
-
-            <div slot="center">
-
-                <div style="display:flex">
-                    <FooterLinkList title="Product">
-                        <a href="/console">Console</a>
-                        <a href="/themes">Themes</a>
-                        <a href="/pricing">Pricing</a>
-                        <a href="/docs">Docs</a>
-                        <a href="/customers">Customers</a>
-                    </FooterLinkList>
-
-                    <FooterLinkList title="Legal">
-                        <a href="/docs/terms">Terms of Service</a>
-                        <a href="/docs/privacy-policy">Privacy Policy</a>
-                    </FooterLinkList>
-
-                    <FooterLinkList title="HYVOR">
-                        <a href="https://hyvor.com" target="_blank">hyvor.com</a>
-                        <a href="https://hyvor.com/about" target="_blank">About</a>
-                        <a href="https://status.hyvor.com" target="_blank">System Status</a>
-                    </FooterLinkList>
-                </div>
-
-            </div>
-
-        </Footer>
+        <Footer />
     </div>
 
 {/if}
---
+++ frontend/src/routes/(marketing)/Footer.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import { FooterLinkList, Footer } from '@hyvor/design/marketing';
+    
+</script>
+
+<Footer
+    email="blogs.support@hyvor.com"
+    social={{
+        x: 'https://x.com/HyvorBlogs',
+        youtube: null,
+        discord: null,
+    }}
+>
+
+
+    <div slot="center">           
+
+        <div class="row first">
+            <FooterLinkList title="Product">
+                <a href="/console">Console</a>
+                <a href="/themes">Themes</a>
+                <a href="/pricing">Pricing</a>
+                <a href="/docs">Docs</a>
+                <a href="/customers">Customers</a>
+            </FooterLinkList>
+
+            <FooterLinkList title="Legal">
+                <a href="/docs/terms">Terms of Service</a>
+                <a href="/docs/privacy-policy">Privacy Policy</a>
+            </FooterLinkList>
+
+            <FooterLinkList title="HYVOR">
+                <a href="https://hyvor.com" target="_blank">hyvor.com</a>
+                <a href="https://hyvor.com/about" target="_blank">About</a>
+                <a href="https://status.hyvor.com" target="_blank">System Status</a>
+            </FooterLinkList>
+        </div>
+
+        <div class="row">
+            <FooterLinkList title="Alternatives">
+                <a href="https://hyvor.com/blog/wordpress-alternatives" target="_blank">WordPress Alternatives</a>
+                <a href="https://hyvor.com/blog/ghost-alternatives" target="_blank">Ghost Alternatives</a>
+                <a href="https://hyvor.com/blog/medium-alternatives" target="_blank">Medium Alternatives</a>
+                <a href="https://hyvor.com/blog/blogger-alternatives" target="_blank">Blogger Alternatives</a>
+            </FooterLinkList>
+        </div>
+
+    </div>
+
+</Footer>
+
+<style>
+    .row {
+        display: flex;
+    }
+    .row:not(.first) {
+        margin-top: 60px;
+    }
+
+    @media (max-width: 992px) {
+        .row {
+            flex-direction: column;
+            gap: 35px;
+        }
+        .row:not(.first) {
+            margin-top: 35px;
+        }
+    }
+
+</style>
