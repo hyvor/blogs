b16ff44ce6d2579fb6d25543e75a70bb501a7de8
SupunKavinda <supunkavinda1125@gmail.com>
2024-06-17 20:24:26 +0200

    comment

--- backend/app/Domains/Blog/Events/BlogUrlChangedEvent.php
+++ backend/app/Domains/Blog/Events/BlogUrlChangedEvent.php
@@ -5,6 +5,10 @@ namespace App\Domains\Blog\Events;
 use App\Models\Blog;
 use Illuminate\Foundation\Events\Dispatchable;
 
+/**
+ * This is a sub-event of BlogUpdatedEvent, not a new one
+ * It listens to BlogUpdatedEvent in CheckBlogUrlChangedListener
+ */
 class BlogUrlChangedEvent
 {
     use Dispatchable;
@@ -14,6 +18,7 @@ class BlogUrlChangedEvent
         public Blog $blogOriginal,
         public string $oldUrl,
         public string $newUrl
-    ) {}
+    ) {
+    }
 
 }
