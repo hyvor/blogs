eedfe98e8a7646d523deb9b34d3588ee8fbe3da0
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2024-06-19 04:02:03 +0530

    test wip

--- backend/tests/Unit/Domains/Webhook/Listeners/WebhookSubscriberTest.php
+++ backend/tests/Unit/Domains/Webhook/Listeners/WebhookSubscriberTest.php
@@ -6,10 +6,22 @@ use App\Domains\Blog\Events\BlogUpdatedEvent;
 use App\Domains\Cache\Events\CacheClearAllEvent;
 use App\Domains\Cache\Events\CacheClearSingleEvent;
 use App\Domains\Cache\Events\CacheClearTemplatesEvent;
+use App\Domains\Language\Events\LanguageChangedEvent;
+use App\Domains\Media\Events\MediaCreatedEvent;
+use App\Domains\Media\Events\MediaDeletedEvent;
 use App\Domains\Post\Events\PostCreatedEvent;
+use App\Domains\Post\Events\PostVariantCreatedEvent;
+use App\Domains\Route\Events\RouteChangedEvent;
+use App\Domains\Route\RouteRepository;
 use App\Domains\Webhook\Jobs\WebhookDeliveryJob;
 use App\Domains\Webhook\Listeners\WebhookSubscriber;
+use App\Models\PostVariant;
+use App\Models\Route;
+use Database\Factories\LanguageFactory;
+use Database\Factories\MediaFactory;
 use Database\Factories\PostFactory;
+use Database\Factories\PostVariantFactory;
+use Database\Factories\RouteFactory;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Queue;
 use App\Data\Enums\WebhookEventEnum;
@@ -59,21 +71,133 @@ it('calls webhook delivery job on post created event', function () {
     Queue::fake();
 
     $blog = blog();
-    $post = PostFactory::new()->create();
+    $post = PostFactory::new()->create([
+        'blog_id' => $blog->id,
+    ]);
     createWebhookFor($blog, WebhookEventEnum::POST_CREATED);
 
     $event = new PostCreatedEvent($post);
     $listener = new WebhookSubscriber();
     $listener->onPostCreatedEvent($event);
 
-    // dd(WebhookDeliveryJob::class);
-    // Queue::assertPushed(fn (WebhookDeliveryJob $job) => $job->delivery->event === WebhookEventEnum::POST_CREATED);
+    Queue::assertPushed(function (WebhookDeliveryJob $job) use ($post){
+        expect($job->delivery->event)->toBe(WebhookEventEnum::POST_CREATED);
+        expect($job->delivery->data['post']['id'])->toBe($post->id);
+        return true;
+    });
+});
 
-    // Queue::assertPushed(function (WebhookDeliveryJob $job) {
-    //     expect($job->delivery->event)->toBe(WebhookEventEnum::POST_CREATED);
-    //     // expect($job->delivery->data['post']['id'])->toBe($post->id);
-    //     return true;
-    // });
+// it('calls webhook delivery job on post variant created event', function () {
+//     Queue::fake();
+
+//     $blog = blog();
+//     $post = PostFactory::new()->create([
+//         'blog_id' => $blog->id,
+//     ]);
+//     $postVariant = PostVariantFactory::new()->create([
+//         'post_id' => $post->id,
+//     ]);
+//     RouteFactory::new()->create([
+//         'blog_id' => $blog->id,
+//         'name' => 'post',
+//     ]);
+
+//     createWebhookFor($blog, WebhookEventEnum::POST_UPDATED);
+
+//     $event = new PostVariantCreatedEvent($postVariant);
+//     $listener = new WebhookSubscriber();
+//     $listener->onPostVariantCreatedEvent($event);
+
+//     Queue::assertPushed(function (WebhookDeliveryJob $job) use ($post){
+//         expect($job->delivery->event)->toBe(WebhookEventEnum::POST_UPDATED);
+//         expect($job->delivery->data['post']['id'])->toBe($post->id);
+//         return true;
+//     });
+
+// });
+
+it('calls webhook delivery job on media created event', function () {
+    Queue::fake();
+
+    $blog = blog();
+    $media = MediaFactory::new()->create([
+        'blog_id' => $blog->id,
+    ]);
+
+    createWebhookFor($blog, WebhookEventEnum::MEDIA_CREATED);
+
+    $event = new MediaCreatedEvent($media);
+    $listener = new WebhookSubscriber();
+    $listener->onMediaCreatedEvent($event);
+
+    Queue::assertPushed(function (WebhookDeliveryJob $job) use ($media){
+        expect($job->delivery->event)->toBe(WebhookEventEnum::MEDIA_CREATED);
+        expect($job->delivery->data['media']['id'])->toBe($media->id);
+        return true;
+    });
+});
+
+it('calls webhook delivery job on media deleted event', function () {
+    Queue::fake();
+    $blog = blog();
+    $media = MediaFactory::new()->create([
+        'blog_id' => $blog->id,
+    ]);
+
+    createWebhookFor($blog, WebhookEventEnum::MEDIA_DELETED);
+
+    $event = new MediaDeletedEvent($media);
+    $listener = new WebhookSubscriber();
+    $listener->onMediaDeletedEvent($event);
+
+    Queue::assertPushed(function (WebhookDeliveryJob $job) use ($media){
+        expect($job->delivery->event)->toBe(WebhookEventEnum::MEDIA_DELETED);
+        expect($job->delivery->data['media']['id'])->toBe($media->id);
+        return true;
+    });
+});
+
+it('calls webhook delivery job on route change event', function () {
+    Queue::fake();
+
+    $blog = blog();
+    $route = RouteFactory::new()->create([
+        'blog_id' => $blog->id,
+        'name' => 'post',
+    ]);
+
+    createWebhookFor($blog, WebhookEventEnum::ROUTES_CHANGED);
+
+    $event = new RouteChangedEvent($route);
+    $listener = new WebhookSubscriber();
+    $listener->onRouteChangedEvent($event);
+
+    Queue::assertPushed(function (WebhookDeliveryJob $job) use ($route){
+        expect($job->delivery->event)->toBe(WebhookEventEnum::ROUTES_CHANGED);
+        expect($job->delivery->data['routes'][0]['id'])->toBe($route->id);
+        return true;
+    });
+});
+
+it('calls webhook delivery job on language changed event', function () {
+    Queue::fake();
+
+    $blog = blog();
+    $language = LanguageFactory::new()->create([
+        'blog_id' => $blog->id,
+    ]);
+
+    createWebhookFor($blog, WebhookEventEnum::LANGUAGES_CHANGED);
+
+    $event = new LanguageChangedEvent($language);
+    $listener = new WebhookSubscriber();
+    $listener->onLanguageChangedEvent($event);
+
+    Queue::assertPushed(function (WebhookDeliveryJob $job) use ($language){
+        expect($job->delivery->event)->toBe(WebhookEventEnum::LANGUAGES_CHANGED);
+        expect($job->delivery->data['languages'][0]['id'])->toBe($language->id);
+        return true;
+    });
 });
 
 it('calls webhook delivery job on cache clear single event', function () {
