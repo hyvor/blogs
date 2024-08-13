ec19f35f414ad9b41bd2ee8586a0760c946eb848
SupunKavinda <supunkavinda1125@gmail.com>
2024-08-13 08:13:04 +0200

    fixes #434

--- frontend/src/routes/console/[subdomain]/posts/[postId]/Sidebar/Settings/PublishTime.svelte
+++ frontend/src/routes/console/[subdomain]/posts/[postId]/Sidebar/Settings/PublishTime.svelte
@@ -1,81 +1,76 @@
 <script lang="ts">
-	import { SplitControl, Text, TextInput } from "@hyvor/design/components";
-	import { postOriginalStore, postOriginalVariantStore, postStore, postVariantStore, updatePostStore } from "../../../postStore";
-	import UnsavedTag from "./UnsavedTag.svelte";
-	import { updatePost } from "../../../postActions";
-	import OnlyPrimaryVariant from "./OnlyPrimaryVariant.svelte";
+	import { SplitControl, Text, TextInput } from '@hyvor/design/components';
+	import {
+		postOriginalStore,
+		postOriginalVariantStore,
+		postStore,
+		postVariantStore,
+		updatePostStore
+	} from '../../../postStore';
+	import UnsavedTag from './UnsavedTag.svelte';
+	import { updatePost } from '../../../postActions';
+	import OnlyPrimaryVariant from './OnlyPrimaryVariant.svelte';
+	import dayjs from 'dayjs';
 
-    let loaderState : 'none' | 'loading' | 'success' | 'error' = 'none';
+	let loaderState: 'none' | 'loading' | 'success' | 'error' = 'none';
 
-    function getUnixTimestamp(date: string) {
-        return Math.floor(new Date(date).getTime() / 1000);
-    }
+	function getUnixTimestamp(date: string) {
+		return Math.floor(new Date(date).getTime() / 1000);
+	}
 
-    function timestampToDateTime(timestamp: number | null) {
-        if (!timestamp)
-            return '';
-        const date = new Date(timestamp * 1000);
-        return date.toISOString().slice(0, 16);
-    }
+	function timestampToDateTime(timestamp: number | null) {
+		if (!timestamp) return '';
+		return dayjs.unix(timestamp).format('YYYY-MM-DDTHH:mm');
+	}
 
-    function handleInput(e: any) {
-        const val = e.target.value;
-        updatePostStore({
-            published_at: val ? getUnixTimestamp(val) : null
-        });
-    }
+	function handleInput(e: any) {
+		const val = e.target.value;
+		updatePostStore({
+			published_at: val ? getUnixTimestamp(val) : null
+		});
+	}
 
-    function handleBlur(e: any) {
-        const val = e.target.value;
-        const timestamp = val ? getUnixTimestamp(val) : null;
-        
-        if (timestamp === $postOriginalStore.published_at)
-            return;
+	function handleBlur(e: any) {
+		const val = e.target.value;
+		const timestamp = val ? getUnixTimestamp(val) : null;
 
-        if ($postVariantStore.status !== 'published') {
+		if (timestamp === $postOriginalStore.published_at) return;
 
-            loaderState = 'loading';
+		if ($postVariantStore.status !== 'published') {
+			loaderState = 'loading';
 
-            updatePost({published_at: timestamp})
-                .then(() => {
-                    loaderState = 'success';
-                })
-                .catch(err => {
-                    loaderState = 'error';
-                })
-        }
-    }
-
+			updatePost({ published_at: timestamp })
+				.then(() => {
+					loaderState = 'success';
+				})
+				.catch((err) => {
+					loaderState = 'error';
+				});
+		}
+	}
 </script>
 
 <OnlyPrimaryVariant>
+	<SplitControl>
+		<span slot="label">
+			Publish Time
 
-    <SplitControl>
-        <span slot="label">
-            Publish Time
+			<UnsavedTag
+				show={$postStore.published_at !== $postOriginalStore.published_at}
+				{loaderState}
+			/>
+		</span>
 
-            <UnsavedTag 
-                show={$postStore.published_at !== $postOriginalStore.published_at}   
-                loaderState={loaderState} 
-            />
-        </span>
-
-        {#if $postVariantStore.status !== 'draft' || $postStore.published_at !== null}
-
-            <TextInput 
-                block
-                type="datetime-local"
-                value={timestampToDateTime($postStore.published_at)}
-                on:input={handleInput}
-                on:blur={handleBlur}
-            />
-
-        {:else}
-            <Text small light>
-                Not published yet
-            </Text>
-        {/if}
-
-    </SplitControl>
-
+		{#if $postVariantStore.status !== 'draft' || $postStore.published_at !== null}
+			<TextInput
+				block
+				type="datetime-local"
+				value={timestampToDateTime($postStore.published_at)}
+				on:input={handleInput}
+				on:blur={handleBlur}
+			/>
+		{:else}
+			<Text small light>Not published yet</Text>
+		{/if}
+	</SplitControl>
 </OnlyPrimaryVariant>
