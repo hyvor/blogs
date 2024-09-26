54a9d9ea1c26e00a1aa09dc828997268dd893c04
SupunKavinda <supunkavinda1125@gmail.com>
2024-09-26 05:01:46 +0200

    test fix

--- backend/tests/Feature/ConsoleAPI/Integrations/HyvorTalk/GetMembershipPlansTest.php
+++ backend/tests/Feature/ConsoleAPI/Integrations/HyvorTalk/GetMembershipPlansTest.php
@@ -9,22 +9,27 @@ use Illuminate\Support\Facades\Http;
 it('gets membership plan names', function() {
 
     Http::fake([
-        'https://talk.hyvor.com/api/internal/blogs/integration/console-api' => function (Request $request) {
-            dd($request->data());
-
-            return Http::response([
+        'https://talk.hyvor.com/api/internal/blogs/integration/console-api' => Http::sequence()
+            ->push([
+                'memberships_enabled' => true,
+                'memberships_currency' => 'USD',
+            ])
+            ->push([
                 [
                     'name' => 'Free',
+                    'monthly_price' => 10,
                 ],
                 [
                     'name' => 'Pro',
+                    'monthly_price' => 20,
                 ],
                 [
                     'name' => 'Business',
+                    'monthly_price' => 30,
                 ]
-            ]);
-        }
+            ])
     ]);
+
 
     $blog = blogWithAccess();
 
@@ -35,13 +40,19 @@ it('gets membership plan names', function() {
 
     consoleApi($blog, 'get', '/integrations/hyvor-talk/membership-plans')
         ->assertOk()
-        ->assertJsonPath('0', 'Free')
-        ->assertJsonPath('1', 'Pro')
-        ->assertJsonPath('2', 'Business');
+        ->assertJsonPath('currency', 'USD')
+        ->assertJsonPath('plans.0.name', 'Free')
+        ->assertJsonPath('plans.1.name', 'Pro')
+        ->assertJsonPath('plans.2.name', 'Business');
 
     Http::assertSent(function (Request $request) {
 
         $data = json_decode(decrypt($request->data()['message'], false), true)['data'];
+
+        if ($data['endpoint'] === '/website') {
+            return true;
+        }
+
         expect($data['endpoint'])->toBe('/membership-plans');
         expect($data['method'])->toBe('GET');
         expect($data['website_id'])->toBe(23);
