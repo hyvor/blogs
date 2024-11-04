f91b3fa96fa419db81dbc5d32d926a2ef261e13e
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2024-11-04 15:00:25 +0530

    fix

--- backend/app/Console/Commands/Import/ImportWordPressCommand.php
+++ backend/app/Console/Commands/Import/ImportWordPressCommand.php
@@ -64,7 +64,7 @@ class ImportWordPressCommand extends Command
             $this->info((string) json_encode([
                 'posts' => count($parser->posts),
                 'uploads' => count($parser->uploads),
-                'duplicates ignored' =>$parser->duplicateCount
+                'duplicates_ignored' =>$parser->duplicateCount
             ]));
 
             return;
