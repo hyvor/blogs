2724a48811b865b8e3df14667c1267ced277646f
Nolab <thibault.boutet@epita.fr>
2025-01-16 08:00:40 -0500

    Handle empty search attribute

--- backend/app/Domains/Post/PostSearchRepository.php
+++ backend/app/Domains/Post/PostSearchRepository.php
@@ -52,7 +52,18 @@ class PostSearchRepository
 
         // Compute FTS on the fly
         $post_variants = PostVariant::
-            whereRaw("to_tsvector(ts_language::regconfig, title || '' || description || '' || slug || '' || content_text) @@ to_tsquery(ts_language::regconfig, ?)", ["$searchQuery:*"])
+        whereRaw("
+            to_tsvector(
+                ts_language::regconfig,
+                COALESCE(title, '') || ' ' || 
+                COALESCE(description, '') || ' ' || 
+                COALESCE(slug, '') || ' ' || 
+                COALESCE(content_text, '')
+                ) @@ to_tsquery(
+                ts_language::regconfig,
+                ?
+            )
+        ", ["$searchQuery:*"])
             ->where('language_id', $language->id)
             ->when($isPublished, function ($query) {
                 $query->where('status', 'published');
