d5bbb64367236801672bacc59898a8f7377b4172
Supun Wimalasena <supun@hyvor.com>
2025-01-22 03:51:21 +0100

    basic full text search functionality

--- backend/database/migrations/2021_09_04_191510_create_post_variants_table.php
+++ backend/database/migrations/2021_09_04_191510_create_post_variants_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
@@ -45,6 +46,26 @@ return new class () extends Migration {
             $table->index('status');
             $table->index('words');
         });
+
+        # add columns for full text search
+        DB::unprepared("
+            ALTER TABLE post_variants ADD COLUMN ts_language regconfig DEFAULT 'simple';
+            
+            ALTER TABLE post_variants
+            ADD COLUMN ts tsvector
+            GENERATED ALWAYS AS
+            (
+                to_tsvector(
+                    ts_language,
+                    COALESCE(title, '') || ' ' ||
+                    COALESCE(description, '') || ' ' ||
+                    COALESCE(slug, '') || ' ' ||
+                    COALESCE(content_text, '') 
+                )
+            ) STORED;
+
+            CREATE INDEX ts_idx ON post_variants USING GIN (ts);
+        ");
     }
 
     /**
