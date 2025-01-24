879b3dcaf97d30b981963f999affc3a94bdb3c10
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-01-24 10:55:42 +0530

    command

---
+++ backend/app/Console/Commands/Migrations/D_2025_01_22_DatabaseMigrationCommand.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Console\Commands\Migrations;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class D_2025_01_22_DatabaseMigrationCommand extends Command
+{
+
+    public $signature = 'migrate:database-pgsql';
+
+    public function handle() : void
+    {
+        $tablesToSkip =  [
+            'migrations',
+            'cache',
+            'cache_locks',
+            'failed_jobs',
+
+            // Pending Issues
+            'theme_files'
+        ];
+
+//        if (!$this->confirm('Are you sure you want to migrate the database?')) {
+//            return;
+//        }
+
+        $tables = DB::select('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\'');
+        $lastRun = null;
+
+        while (true){
+            $currentRun = now()->toImmutable();
+            $recordsProcessed = false;
+
+            foreach ($tables as $table) {
+
+                if (in_array($table->table_name, $tablesToSkip)) {
+                    $this->info("Skipping table: {$table->table_name}");
+                    continue;
+                }
+
+                $this->info("Migrating table: {$table->table_name}");
+
+                // Get the columns of the current table
+                $columns = DB::select('SELECT column_name FROM information_schema.columns WHERE table_name = ?', [$table->table_name]);
+                $columnNames = array_map(fn($column) => $column->column_name, $columns);
+
+
+                DB::connection('mysql')
+                    ->table($table->table_name)
+                    ->when($lastRun, fn($query) => $query->where('updated_at', '>=', $lastRun))
+                    ->orderBy('id')
+                    ->chunk(1000, function ($rows) use ($table, $columnNames, &$recordsProcessed) {
+
+                        if ($rows->isNotEmpty()) {
+                            $recordsProcessed = true;
+                        }
+
+                        foreach ($rows as $row) {
+                            $data = (array) $row;
+
+                            // Filter data to only include existing columns
+                            $filteredData = array_filter($data, fn($key) => in_array($key, $columnNames), ARRAY_FILTER_USE_KEY);
+
+                            DB::connection('pgsql')
+                                ->table($table->table_name)
+                                ->updateOrInsert(
+                                    ['id' => $data['id']],
+                                    $filteredData
+                                );
+                        }
+                    });
+            }
+
+            if (!$recordsProcessed) {
+                break;
+            }
+
+            $lastRun = clone $currentRun;
+            sleep(1);
+        }
+    }
+}
