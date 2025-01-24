339835ebd88f81a9c1a0d25b083fb6b3fe8841e5
Supun Wimalasena <supun@hyvor.com>
2025-01-24 13:47:57 +0100

    export subscriptions

--- backend/app/Models/Subscription.php
+++ backend/app/Models/Subscription.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use App\Data\Enums\SubscriptionFrequencyEnum;
 use App\Data\Enums\SubscriptionPlanEnum;
 use App\Data\Enums\SubscriptionStatusEnum;
-use Carbon\Carbon;
 use Hyvor\JsonMeta\Definer;
 use Hyvor\JsonMeta\Metable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Subscription extends Model
 {
@@ -23,6 +22,14 @@ class Subscription extends Model
         'status' => SubscriptionStatusEnum::class,
         'ends_at' => 'datetime'
     ];
+
+    /**
+     * @return BelongsTo<Blog, $this>
+     */
+    public function blog(): BelongsTo
+    {
+        return $this->belongsTo(Blog::class);
+    }
 
     public function metaDefinition(Definer $definer) : void
     {
