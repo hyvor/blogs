272d5c9186f3defd3cc73f4fa266e6df33e67f8a
Supun Wimalasena <supun@hyvor.com>
2025-02-17 01:06:01 +0100

    editor page

---
+++ frontend/src/routes/editor/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Prosemirror from '../console/(nav)/[subdomain]/posts/[postId]/Body/Editor/Prosemirror.svelte';
+</script>
+
+<div class="wrap hds-box">
+	<Prosemirror value={null} />
+</div>
+
+<style>
+	.wrap {
+		width: 650px;
+		margin: 20px auto;
+		height: calc(100vh - 40px);
+	}
+</style>
