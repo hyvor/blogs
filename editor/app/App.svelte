371345750ccfdcfae3bad52803ef4dc52995b538
Supun Wimalasena <supun@hyvor.com>
2025-02-17 02:40:38 +0100

    wip

--- editor/app/App.svelte
+++ editor/app/App.svelte
@@ -2,4 +2,14 @@
   import { Editor } from "../src";
 </script>
 
-<Editor value={null} />
+<div class="container">
+  <Editor value={null} />
+</div>
+
+<style>
+  .container {
+    margin: 60px auto;
+    height: 100vh;
+    width: 650px;
+  }
+</style>
