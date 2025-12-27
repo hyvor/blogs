8e7659b8b429e0bd37e2922cc57f0fb263a14faa
Nadil Karunarathna <113877141+Nadil-K@users.noreply.github.com>
2025-12-27 18:02:30 +0530

    Logging for custom domain blogs (#716)

--- backend/app/Http/Controllers/Special/CaddyController.php
+++ backend/app/Http/Controllers/Special/CaddyController.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Special;
 
 use App\Domains\Blog\BlogService;
-use App\Exceptions\TrustedException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class CaddyController
 {
     public function checkDomain(Request $request): Response
     {
+        Log::info("Info from CaddyController/checkDomain", [
+            "domain" => $request->input('domain'),
+            "host" => $request->getHost(),
+            "url" => $request->getBaseUrl()
+        ]);
+
         $request->validate([
             'domain' => 'required|string',
         ]);
