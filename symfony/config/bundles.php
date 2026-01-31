19c3c324d0880ecd9961fe1edf7f8feea0e43328
Supun Wimalasena <supun@hyvor.com>
2026-01-31 13:42:03 +0100

    install internal library

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Hyvor\Internal\Bundle\InternalBundle::class => ['all' => true],
 ];
