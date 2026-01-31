8622b39c1cb5cac190a407faa9f63a7cdc2caf2d
Supun Wimalasena <supun@hyvor.com>
2026-01-31 16:40:55 +0100

    test wip

--- symfony/tests/Service/User/Comms/MemberRemovedListenerTest.php
+++ symfony/tests/Service/User/Comms/MemberRemovedListenerTest.php
@@ -2,4 +2,18 @@
 
 use Hyvor\Internal\Bundle\Testing\KernelTestCase;
 
-class MemberRemovedListenerTest extends KernelTestCase {}
+class MemberRemovedListenerTest extends KernelTestCase
+{
+
+    public function test_deletes_user_and_variants(): void
+    {
+        $blog = \App\Tests\Factory\BlogFactory::createOne();
+
+        $user = \App\Tests\Factory\UserFactory::createOne([
+            '',
+        ]);
+
+        $this->getEd();
+    }
+
+}
