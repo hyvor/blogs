54ac6731a3f819507a6bce03a9ac9e3aa235b68b
Supun Wimalasena <supun@hyvor.com>
2026-06-01 22:03:02 +0200

    monolog and sentry

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Messenger\Test\ZenstruckMessengerTestBundle::class => ['test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
