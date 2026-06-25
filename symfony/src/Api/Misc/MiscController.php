17534155dcba8277926dca1665982e0a0864cf6b
Supun Wimalasena <supun@hyvor.com>
2026-06-25 20:32:49 +0200

    repo sync (initial)

--- symfony/src/Api/Misc/MiscController.php
+++ symfony/src/Api/Misc/MiscController.php
@@ -2,18 +2,34 @@
 
 namespace App\Api\Misc;
 
+use App\Service\Theme\ThemeService;
 use League\Flysystem\Filesystem;
 use League\Flysystem\UnableToCheckExistence;
 use League\Flysystem\UnableToReadFile;
 use League\Flysystem\UnableToRetrieveMetadata;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MiscController extends AbstractController
 {
 
-    public function __construct(private Filesystem $filesystem) {}
+    public function __construct(
+        private Filesystem $filesystem,
+        private ThemeService $themeService,
+    ) {}
+
+    #[Route('/api/misc/themes', methods: ['GET'])]
+    public function getAllThemes(): JsonResponse
+    {
+        $themes = $this->themeService->getAllThemesWithLatestVersions();
+
+        return new JsonResponse(array_map(
+            fn($row) => new ThemeObject($row['theme'], $row['latest_version']),
+            $themes,
+        ));
+    }
 
     #[Route('/api/health', methods: ['GET'])]
     public function healthCheck(): Response
