c9370b46091d5d7ebb50d77911e1634d3a4160b6
Supun Wimalasena <supun@hyvor.com>
2026-07-08 20:14:31 +0200

    more cleanup

--- backend/routes/api-console.php
+++ backend/routes/api-console.php
@@ -2,27 +2,10 @@
 
 declare(strict_types=1);
 
-use App\Http\ConsoleApi\Middleware\ConsoleApiAuthMiddleware;
 use App\Http\Controllers\ConsoleAPI\ConsoleAiController;
-use App\Http\Controllers\ConsoleAPI\ConsoleDangerController;
-use App\Http\Controllers\ConsoleAPI\ConsoleExportController;
 use App\Http\Controllers\ConsoleAPI\ConsoleGptController;
-use App\Http\Controllers\ConsoleAPI\ConsoleLinkAnalysisController;
-use App\Http\Controllers\ConsoleAPI\ConsoleUserBlogController;
-use App\Http\Controllers\ConsoleAPI\Import\ConsoleImportController;
-use App\Http\Controllers\ConsoleAPI\Import\ConsoleImportSitemapController;
-use App\Http\Middleware\App\ConsoleApi\ConsoleApiAccessMiddleware;
-use App\Http\Middleware\App\ConsoleApi\ResourceAccessMiddleware;
-use App\Http\Middleware\App\SubdomainMiddleware;
-use App\Http\Middleware\CorsOnLocalhost;
-use Illuminate\Support\Facades\Route;
 
 Route::prefix('/api/console/v0/blog/{subdomain}')
-    ->middleware([
-        SubdomainMiddleware::class,
-        ConsoleApiAccessMiddleware::class,
-        ResourceAccessMiddleware::class,
-    ])
     ->group(function () {
         /**
          * Posts and media
