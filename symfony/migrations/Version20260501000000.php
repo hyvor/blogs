a1067097b9f80b6a14ae792a6b57d625a73ea6ce
Supun Wimalasena <supun@hyvor.com>
2026-08-07 00:57:40 +0200

    zenstruct messenger monitor

--- symfony/migrations/Version20260501000000.php
+++ symfony/migrations/Version20260501000000.php
@@ -179,6 +179,30 @@ final class Version20260501000000 extends AbstractMigration
             item_time int4 NOT NULL
         );
         SQL);
+
+        // for zenstruct/messenger-monitor-bundle
+        $this->addSql(
+            <<<SQL
+            CREATE TABLE messenger_processed_messages (
+                id SERIAL PRIMARY KEY,
+                run_id INT NOT NULL,
+                attempt INT NOT NULL DEFAULT 1,
+                message_type VARCHAR(255) NOT NULL,
+                description TEXT,
+                dispatched_at TIMESTAMP WITH TIME ZONE NOT NULL,
+                received_at TIMESTAMP WITH TIME ZONE NOT NULL,
+                finished_at TIMESTAMP WITH TIME ZONE NOT NULL,
+                memory_usage INT NOT NULL,
+                transport VARCHAR(100) NOT NULL,
+                tags TEXT,
+                wait_time INT NOT NULL,
+                handle_time INT NOT NULL,
+                failure_type VARCHAR(255),
+                failure_message TEXT,
+                results JSONB
+            );
+            SQL
+        );
     }
 
     public function down(Schema $schema): void {}
