e1892298bd0c37efb00dc8ad86d72447fc9d181d
Supun Wimalasena <supun@hyvor.com>
2026-08-09 20:12:25 +0200

    caddy get certificate endpoint done

--- symfony/src/Api/Local/LocalController.php
+++ symfony/src/Api/Local/LocalController.php
@@ -4,6 +4,7 @@ namespace App\Api\Local;
 
 use App\Service\Hosting\CustomDomain\CustomDomainService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,11 +19,20 @@ class LocalController extends AbstractController
     /**
      * This provides Caddy with the certificate for the given custom domain.
      * https://caddyserver.com/docs/caddyfile/directives/tls#http-1
+     *
+     * Security: we have to absolutely be sure that this endpoint is only accessible by Caddy locally.
+     * This is secured by the PHP env variable that Caddyfile sets (caddy-router=local)
+     * In addition, we check the IP just in case we make a mistake in the Caddyfile.
      */
     #[Route('/api/local/caddy-certificate', methods: ['GET'])]
     public function getCaddyCertificate(
-        #[MapQueryParameter] string $server_name
+        #[MapQueryParameter] string $server_name,
+        Request $request,
     ): Response {
+        if ($request->getClientIp() !== '127.0.0.1') {
+            return new Response('forbidden', 403);
+        }
+
         $customDomain = $this->customDomainService->getCustomDomain($server_name);
 
         if ($customDomain === null) {
---
+++ symfony/tests/Api/Local/GetCaddyCertificateTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Tests\Api\Local;
+
+use App\Api\Local\LocalController;
+use App\Tests\Case\ApiTestCase;
+use App\Tests\Factory\CustomDomainFactory;
+use Hyvor\Internal\Util\Crypt\Encryption;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(LocalController::class)]
+class GetCaddyCertificateTest extends ApiTestCase
+{
+
+    public function test_cannot_call_from_non_local_ip(): void
+    {
+        $this->setEnvVar('CADDY_ROUTER', 'local');
+        $this->client->request('GET', '/api/local/caddy-certificate?server_name=example.com', [], [], [
+            'REMOTE_ADDR' => '1.1.1.1',
+        ]);
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function test_when_domain_not_found(): void
+    {
+        $this->setEnvVar('CADDY_ROUTER', 'local');
+        $this->client->request('GET', '/api/local/caddy-certificate?server_name=example.com', [], [], [
+            'REMOTE_ADDR' => '127.0.0.1',
+        ]);
+        $this->assertResponseStatusCodeSame(404);
+        $this->assertStringContainsString(
+            'domain not found',
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    public function test_when_certificate_not_found(): void
+    {
+        CustomDomainFactory::createOne([
+            'domain' => 'example.com',
+            'certificate' => null,
+            'private_key_encrypted' => null,
+        ]);
+
+
+        $this->setEnvVar('CADDY_ROUTER', 'local');
+        $this->client->request('GET', '/api/local/caddy-certificate?server_name=example.com', [], [], [
+            'REMOTE_ADDR' => '127.0.0.1',
+        ]);
+        $this->assertResponseStatusCodeSame(404);
+        $this->assertStringContainsString(
+            'certificate not found',
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    public function test_returns_certificate_and_private_key(): void
+    {
+        CustomDomainFactory::createOne([
+            'domain' => 'example.com',
+            'certificate' => 'cert',
+            'private_key_encrypted' => $this->getService(Encryption::class)->encryptString('private-key')
+        ]);
+
+        $this->setEnvVar('CADDY_ROUTER', 'local');
+        $this->client->request('GET', '/api/local/caddy-certificate?server_name=example.com', [], [], [
+            'REMOTE_ADDR' => '127.0.0.1',
+        ]);
+        $this->assertResponseIsSuccessful("cert\nprivate-key");
+        $this->assertResponseHeaderSame('Content-Type', 'application/x-pem-file');
+    }
+}
