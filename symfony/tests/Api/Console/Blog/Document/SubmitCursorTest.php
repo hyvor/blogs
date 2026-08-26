f1465d2e86ed53980e2ce37d0b5ee472764a90e2
Supun Wimalasena <supun@hyvor.com>
2026-08-26 23:26:49 +0200

    submit cursor done

--- symfony/tests/Api/Console/Blog/Document/SubmitCursorTest.php
+++ symfony/tests/Api/Console/Blog/Document/SubmitCursorTest.php
@@ -2,20 +2,33 @@
 
 namespace App\Tests\Api\Console\Blog\Document;
 
+use App\Api\Console\Controller\DocumentsController;
+use App\Service\Post\Document\DocumentService;
 use App\Tests\Case\ApiTestCase;
 use App\Tests\Factory\BlogFactory;
 use App\Tests\Factory\LanguageFactory;
 use App\Tests\Factory\PostFactory;
 use App\Tests\Factory\PostVariantFactory;
+use App\Tests\Factory\UserVariantFactory;
 use App\Tests\Fake\FakeHub;
+use PHPUnit\Framework\Attributes\CoversClass;
 
+#[CoversClass(DocumentsController::class)]
+#[CoversClass(DocumentService::class)]
 class SubmitCursorTest extends ApiTestCase
 {
 
     public function test_submit_cursor(): void
     {
-        [$blog, $user] = BlogFactory::createOneWithUser();
+        [$blog, $user] = BlogFactory::createOneWithUser(userAttrs: [
+            'cursor_color' => '#ff0000',
+        ]);
         $language = LanguageFactory::createOnePrimaryFor($blog);
+        UserVariantFactory::createOne([
+            'user' => $user,
+            'language' => $language,
+            'name' => 'Test User',
+        ]);
         $post = PostFactory::createOneFor($blog);
         $variant = PostVariantFactory::createOneFor($post, language: $language);
 
@@ -40,7 +53,17 @@ class SubmitCursorTest extends ApiTestCase
         $this->assertCount(1, $updates);
 
         $update = $updates[0];
-        dd($update->getData());
+        $this->assertSame(json_encode([
+            'type' => 'cursor',
+            'client_id' => 'test-client-id',
+            'from' => 0,
+            'to' => 5,
+            'user' => [
+                'name' => 'Test User',
+                'color' => '#ff0000',
+                'picture' => null,
+            ],
+        ]), $update->getData());
     }
 
 }
