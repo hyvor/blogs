aa0ca2e6e78bc4e625475e7ba531037a0aba9d92
Supun Wimalasena <supun@hyvor.com>
2026-08-30 00:10:05 +0200

    same as prev commit :)

--- symfony/src/Service/Post/Content/PostContentService.php
+++ symfony/src/Service/Post/Content/PostContentService.php
@@ -10,16 +10,6 @@ use Hyvor\Phrosemirror\Exception\PhrosemirrorException;
 class PostContentService
 {
 
-    public const array DEFAULT_CONTENT = [
-        'type' => 'doc',
-        'content' => [
-            [
-                'type' => 'paragraph',
-                'content' => [],
-            ],
-        ],
-    ];
-
     public const string DEFAULT_CONTENT_JSON = '{"type":"doc","content":[{"type":"paragraph","content":[]}]}';
 
     public function __construct(
